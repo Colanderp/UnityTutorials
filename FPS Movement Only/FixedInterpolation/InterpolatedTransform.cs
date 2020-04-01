5b00abb3375cc9876a9f12fa7b2f81e4cf4f00cd
FriskeKC31 <50305955+FriskeKC31@users.noreply.github.com>
2020-03-31 23:31:24 -0500

    Added Teleport Example
    Put TeleportBackHelper.cs on a Collider as a Trigger and it will teleport any interpolated transform

--- FPS Movement Only/FixedInterpolation/InterpolatedTransform.cs
+++ FPS Movement Only/FixedInterpolation/InterpolatedTransform.cs
@@ -13,13 +13,14 @@ using System.Collections;
  */
 public class InterpolatedTransform : MonoBehaviour
 {
-    private Vector3[] m_lastPositions; // Stores the transform of the object from the last two FixedUpdates
-    private int m_newTransformIndex; // Keeps track of which index is storing the newest value.
+    public Vector3[] m_lastPositions; // Stores the transform of the object from the last two FixedUpdates
+    [HideInInspector]
+    public int m_newTransformIndex; // Keeps track of which index is storing the newest value.
 
     /*
      * Initializes the list of previous orientations
      */
-    void OnEnable()
+    public virtual void OnEnable()
     {
         ForgetPreviousTransforms();
     }
@@ -28,17 +29,27 @@ public class InterpolatedTransform : MonoBehaviour
      * Resets the previous transform list to store only the objects's current transform. Useful to prevent
      * interpolation when an object is teleported, for example.
      */
-    public void ForgetPreviousTransforms()
+    public virtual void ForgetPreviousTransforms()
     {
         m_lastPositions = new Vector3[2];
         m_newTransformIndex = 0;
+    }
+
+    public virtual void ResetPositionTo(Vector3 resetTo)
+    {
+        //Remove old interpolation
+        ForgetPreviousTransforms();
+        //Reset position to 'resetTo'
+        transform.position = resetTo;
+        m_lastPositions[m_newTransformIndex] = transform.position;
+        m_lastPositions[OldTransformIndex()] = transform.position;
     }
 
     /*
      * Sets the object transform to what it was last FixedUpdate instead of where is was last interpolated to,
      * ensuring it is in the correct position for gameplay scripts.
      */
-    void FixedUpdate()
+    public virtual void FixedUpdate()
     {
         Vector3 mostRecentTransform = m_lastPositions[m_newTransformIndex];
         transform.position = mostRecentTransform;
@@ -47,7 +58,7 @@ public class InterpolatedTransform : MonoBehaviour
     /*
      * Runs after ofther scripts to save the objects's final transform.
      */
-    public void LateFixedUpdate()
+    public virtual void LateFixedUpdate()
     {
         m_newTransformIndex = OldTransformIndex(); // Set new index to the older stored transform.
         m_lastPositions[m_newTransformIndex] = transform.position;
@@ -56,7 +67,7 @@ public class InterpolatedTransform : MonoBehaviour
     /*
      * Interpolates the object transform to the latest FixedUpdate's transform
      */
-    void Update()
+    public virtual void Update()
     {
         Vector3 newestTransform = m_lastPositions[m_newTransformIndex];
         Vector3 olderTransform = m_lastPositions[OldTransformIndex()];
@@ -67,7 +78,7 @@ public class InterpolatedTransform : MonoBehaviour
     /*
      * The index of the older stored transform
      */
-    private int OldTransformIndex()
+    public virtual int OldTransformIndex()
     {
         return (m_newTransformIndex == 0 ? 1 : 0);
     }
